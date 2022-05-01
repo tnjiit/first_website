3cc3c888910f749e2fd5b58696dd03005d4c01bd
tusharjain <tnjiitr@gmail.com>
2022-04-30 22:38:26 -0700

    Enabling listening.
    Signed-off-by: tusharjain <tnjiitr@gmail.com>

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
+const port = 3000
+
 console.log("Hello World!");
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.listen(port, () => {
+    console.log(`Server Listening on Port ${port}`);
+})
