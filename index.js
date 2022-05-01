622f51a155244c69626bdec71bc09590b525b965
tusharjain <tnjiitr@gmail.com>
2022-05-01 00:01:57 -0700

    FCC Exercise Last
    Signed-off-by: tusharjain <tnjiitr@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
@@ -10,6 +11,7 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     console.log('Saw a request');
@@ -19,6 +21,12 @@ app.get('/', (req, res) => {
 app.get('/json', (req, res) => {
     res.json({
         message: "Hello json"
+    });
+});
+
+app.post('/', (req, res) => {
+    res.json({
+        name: `${req.body.first} ${req.body.last}`
     });
 });
 
