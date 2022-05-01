42a2b00059f7eada25073279224a408374f28e2b
tusharjain <tnjiitr@gmail.com>
2022-04-30 23:12:04 -0700

    Updating to make vercel happy.
    Signed-off-by: tusharjain <tnjiitr@gmail.com>

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
-const port = 3000
+const port = process.env.PORT || 8080;
 
 console.log("Hello World!");
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    console.log('Saw a request');
+    res.sendFile(__dirname + '../views/index.html');
 });
 
 app.listen(port, () => {
     console.log(`Server Listening on Port ${port}`);
-})
+});
