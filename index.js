424047c264e3b902469b8e1e3e828cb54f604bf5
tusharjain <tnjiitr@gmail.com>
2022-04-30 23:44:19 -0700

    FCC Exercise 5
    Summary:
    1. Adding json path to serve json
    2. Adding middleware to log few things.
    
    Signed-off-by: tusharjain <tnjiitr@gmail.com>

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const port = process.env.PORT || 8080;
 console.log("Hello World!");
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
 app.get('/', (req, res) => {
     console.log('Saw a request');
     res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
 });
 
 app.listen(port, () => {
